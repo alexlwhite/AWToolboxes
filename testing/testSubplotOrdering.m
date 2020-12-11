4018a526a979a458cbc00d225c006443099ae533
Alex L. White <alexlw@uw.edu>
2020-12-10 23:40:38 -0700

    blah

--- testing/testSubplotOrdering.m
+++ testing/testSubplotOrdering.m
@@ -1,16 +1,24 @@
-nRows = 3;
-nCols = 3; 
+nRows = 8;
+nCols = 8; 
 
 nC = nRows*nCols;
 
 figure;
 for c = 1:nC
-    subplot(3,3,c); 
-    text(1,1,sprintf('%i',c)); 
+    
+    subplot(nRows, nCols, c); 
+    text(0,0,sprintf('%i',c)); 
     
     [colI, rowI] = ind2sub([nCols nRows], c);
     
-    text(0, 0, sprintf('row=%i, col=%i', rowI, colI));
+    %text(0, 0, sprintf('row=%i, col=%i', rowI, colI));
     
     xlim([-2 2]); ylim([-2 2]);
+    axis off;
 end
+
+%% 
+figure; 
+subplot(nRows, nCols, [1 5 9]);
+text(0, 0, '[1 5 9]');
+
