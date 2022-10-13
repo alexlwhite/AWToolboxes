88ccf6c14b9c1b6a7a4001351b59c0050a3de675
Alex L. White <alwhite@barnard.edu>
2022-10-13 17:39:00 -0400

    added comments and simulations to help understand relationship between PC and Ag

--- SDT/AgToDprime.m
+++ SDT/AgToDprime.m
@@ -12,7 +12,7 @@ dprime = sqrt(2)*norminv(Ag);
 % % 
 % %classic SDT model, assuming a value of d', the separation of two distributions with unit variance 
 % %values of dprime: 
-% ds =  0:0.1:5; 
+% ds =  0:0.1:7; 
 % 
 % %then we vary criterion and compute hit and false alarm rates along the way
 % cs = 10:(-0.01):-10; 
@@ -22,11 +22,16 @@ dprime = sqrt(2)*norminv(Ag);
 % 
 % %Compute Ag corresponding to each value of d'
 % Ags = NaN(1,length(ds));
+% PCs = Ags;
 % for di = 1:length(ds)
 %     %hit rates given this value of dprime   
 %     hrs = 1-normcdf(cs,ds(di),1);
 %     %the compute area under the curve by summing up area of the trapezoids:
 %     Ags(di) = computeAROC(hrs,frs);
+%     %proportion correct with neutral criterion
+%     neutralHR = 1-normcdf(ds(di)/2, ds(di), 1); %hit ratewhen c is midway between 0 and d'
+%     neutralCR = normcdf(ds(di)/2, 0, 1); %correct reject rate 
+%     PCs(di) = mean([neutralHR neutralCR]); %proportion correct is mean of HR and CR
 % end
 % 
 % %Now, from basic SDT math, we have a way to convert from p(corr) to
@@ -35,17 +40,21 @@ dprime = sqrt(2)*norminv(Ag);
 % %   d' = sqrt(2)*z(pc)
 % % and  applies for 2-interval forced-choice experiments. The sqrt(2) factor basically corrects for the fact that 
 % % the observer has 2x as much information. z(x) is the same as matlab's
-% % norminv(x), inverse of normCDF. 
+% % norminv(x), inverse of normCDF. (But why do we use the formula for 2IFC here? Perhaps because 1 stim can give evidence for either option)
 % % 
-% %Can we treat Ag (area under curve) just like 2AFC p(corr), to translate from 
+% %Can we treat Ag (area under curve) just like 2IFC p(corr), to translate from 
 % % from Ag to dprime? 
 % predDs = sqrt(2)*norminv(Ags); %convert to d' (assuming Ag is like p(corr))
 % predAgs = normcdf(ds/sqrt(2)); %convert dprime to Ag also
 % 
+% predPC = normcdf(ds/2);
+% 
 % figure;  hold on;
 % plot(ds,Ags,'b-','LineWidth',4);
 % plot(predDs,predAgs,'g-','LineWidth',2);
-% 
+% plot(ds, PCs, 'r-', 'LineWidth', 4);
+% plot(ds, predPC, 'y-', 'LineWidth', 1.5);
+% legend({'true Ag', 'predicted Ag','true PC','predicted PC'},'Location', 'NorthWest');
 % xlabel('d-prime'); 
 % ylabel('Ag');
 
@@ -53,3 +62,9 @@ dprime = sqrt(2)*norminv(Ag);
 %curve) and dprime as follows: 
 %% dprime = sqrt(2)*norminv(Ag)
 %% Ag = normcdf(dprime/sqrt(2))
+
+%% But note that the relationship between dprime and a one-interval,
+%2-alternative forced-choice proportion correct is different! (assuming
+%unbiased criterion)
+%dprime = 2*norminv(PC);
+%PC = normcdf(dprime/2);
--- SDT/DPrimeToAg.m
+++ SDT/DPrimeToAg.m
@@ -6,12 +6,12 @@ function Ag = DPrimeToAg(dprime)
 Ag = normcdf(dprime/sqrt(2));
 
 %% What's the relationship betwen dprime and area under the ROC curve? 
-% %First let's test out computing area under the curve, Ag, for a range of d'
+%First let's test out computing area under the curve, Ag, for a range of d'
 % %values. 
 % % 
 % %classic SDT model, assuming a value of d', the separation of two distributions with unit variance 
 % %values of dprime: 
-% ds =  0:0.1:5; 
+% ds =  0:0.1:7; 
 % 
 % %then we vary criterion and compute hit and false alarm rates along the way
 % cs = 10:(-0.01):-10; 
@@ -21,11 +21,16 @@ Ag = normcdf(dprime/sqrt(2));
 % 
 % %Compute Ag corresponding to each value of d'
 % Ags = NaN(1,length(ds));
+% PCs = Ags;
 % for di = 1:length(ds)
 %     %hit rates given this value of dprime   
 %     hrs = 1-normcdf(cs,ds(di),1);
 %     %the compute area under the curve by summing up area of the trapezoids:
 %     Ags(di) = computeAROC(hrs,frs);
+%     %proportion correct with neutral criterion
+%     neutralHR = 1-normcdf(ds(di)/2, ds(di), 1); %hit ratewhen c is midway between 0 and d'
+%     neutralCR = normcdf(ds(di)/2, 0, 1); %correct reject rate 
+%     PCs(di) = mean([neutralHR neutralCR]); %proportion correct is mean of HR and CR
 % end
 % 
 % %Now, from basic SDT math, we have a way to convert from p(corr) to
@@ -34,17 +39,21 @@ Ag = normcdf(dprime/sqrt(2));
 % %   d' = sqrt(2)*z(pc)
 % % and  applies for 2-interval forced-choice experiments. The sqrt(2) factor basically corrects for the fact that 
 % % the observer has 2x as much information. z(x) is the same as matlab's
-% % norminv(x), inverse of normCDF. 
+% % norminv(x), inverse of normCDF. (But why do we use the formula for 2IFC here? Perhaps because 1 stim can give evidence for either option)
 % % 
-% %Can we treat Ag (area under curve) just like 2AFC p(corr), to translate from 
+% %Can we treat Ag (area under curve) just like 2IFC p(corr), to translate from 
 % % from Ag to dprime? 
 % predDs = sqrt(2)*norminv(Ags); %convert to d' (assuming Ag is like p(corr))
 % predAgs = normcdf(ds/sqrt(2)); %convert dprime to Ag also
 % 
+% predPC = normcdf(ds/2);
+% 
 % figure;  hold on;
 % plot(ds,Ags,'b-','LineWidth',4);
 % plot(predDs,predAgs,'g-','LineWidth',2);
-% 
+% plot(ds, PCs, 'r-', 'LineWidth', 4);
+% plot(ds, predPC, 'y-', 'LineWidth', 1.5);
+% legend({'true Ag', 'predicted Ag','true PC','predicted PC'},'Location', 'NorthWest');
 % xlabel('d-prime'); 
 % ylabel('Ag');
 
@@ -52,3 +61,9 @@ Ag = normcdf(dprime/sqrt(2));
 %curve) and dprime as follows: 
 %% dprime = sqrt(2)*norminv(Ag)
 %% Ag = normcdf(dprime/sqrt(2))
+
+%% But note that the relationship between dprime and a one-interval,
+%2-alternative forced-choice proportion correct is different! (assuming
+%unbiased criterion)
+%dprime = 2*norminv(PC);
+%PC = normcdf(dprime/2);
