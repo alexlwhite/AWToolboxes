74b168155f1ffae9793d0860daa491337c3a47b4
Alex L. White <alwhite@barnard.edu>
2022-10-13 18:32:03 -0400

    new function to go from area under ROC curve to estimate of unbiased prop correct

---
+++ SDT/AgToUnbiasedPC.m
@@ -0,0 +1,86 @@
+%% function PC = AgToUnbiasedPC(Ag) 
+% Converts between area under the ROC curve (Ag) to an estimated of unbiased proportion correct, for 2AFC task. 
+% Treats Ag like unbiased p(correct) in a 2AFC task. 
+function PC = AgToUnbiasedPC(Ag) 
+
+PC = normcdf(sqrt(2)*norminv(Ag)/2);
+
+%% What's the relationship betwen dprime and area under the ROC curve? 
+% %First let's test out computing area under the curve, Ag, for a range of d'
+% %values. 
+% % 
+% %classic SDT model, assuming a value of d', the separation of two distributions with unit variance 
+% %values of dprime: 
+% ds =  0:0.1:7; 
+% 
+% %then we vary criterion and compute hit and false alarm rates along the way
+% cs = 10:(-0.01):-10; 
+% 
+% %false alarm rates (don't depend on d')
+% frs = 1-normcdf(cs,0,1); 
+% 
+% %Compute Ag corresponding to each value of d'
+% Ags = NaN(1,length(ds));
+% PCs = Ags;
+% for di = 1:length(ds)
+%     %hit rates given this value of dprime   
+%     hrs = 1-normcdf(cs,ds(di),1);
+%     %the compute area under the curve by summing up area of the trapezoids:
+%     Ags(di) = computeAROC(hrs,frs);
+%     %proportion correct with neutral criterion
+%     neutralHR = 1-normcdf(ds(di)/2, ds(di), 1); %hit ratewhen c is midway between 0 and d'
+%     neutralCR = normcdf(ds(di)/2, 0, 1); %correct reject rate 
+%     PCs(di) = mean([neutralHR neutralCR]); %proportion correct is mean of HR and CR
+% end
+% 
+% %Now, from basic SDT math, we have a way to convert from p(corr) to
+% %d-prime, assuming neutral criterion. 
+% %The formula is:
+% %   d' = sqrt(2)*z(pc)
+% % and  applies for 2-interval forced-choice experiments. The sqrt(2) factor basically corrects for the fact that 
+% % the observer has 2x as much information. z(x) is the same as matlab's
+% % norminv(x), inverse of normCDF. (But why do we use the formula for 2IFC here? Perhaps because 1 stim can give evidence for either option)
+% % 
+% %Can we treat Ag (area under curve) just like 2IFC p(corr), to translate from 
+% % from Ag to dprime? 
+% predDs = sqrt(2)*norminv(Ags); %convert to d' (assuming Ag is like p(corr))
+% predAgs = normcdf(ds/sqrt(2)); %convert dprime to Ag also
+% 
+% predPC = normcdf(ds/2);
+% 
+% figure;  hold on;
+% plot(ds,Ags,'b-','LineWidth',4);
+% plot(predDs,predAgs,'g-','LineWidth',2);
+% plot(ds, PCs, 'r-', 'LineWidth', 4);
+% plot(ds, predPC, 'y-', 'LineWidth', 1.5);
+% legend({'true Ag', 'predicted Ag','true PC','predicted PC'},'Location', 'NorthWest');
+% xlabel('d-prime'); 
+% ylabel('Ag');
+% 
+% %% % So, yes, we can summarize the relationship between Ag (area under ROC curve) and dprime as follows: 
+% %% dprime = sqrt(2)*norminv(Ag)
+% %% Ag = normcdf(dprime/sqrt(2))
+% 
+% %% But note that the relationship between dprime and a one-interval, 2-AFC proportion correct is different!
+% % (assuming unbiased criterion)
+% %
+% % dprime = 2*norminv(PC);
+% % PC = normcdf(dprime/2);
+% % 
+% %% So, the relationship between PC and Ag is...  
+% %% 2*norminv(PC) = sqrt(2)*norminv(Ag);
+% %% PC = normcdf(sqrt(2)*norminv(Ag)/2);
+% % 
+% % proportion correct is lower than Ag
+% predPC2 = normcdf(sqrt(2)*norminv(Ags)/2);
+% figure; hold on
+% plot([0.5 1],[0.5 1],'k-')
+% h1=plot(Ags, PCs, 'b-','LineWidth', 4);
+% h2=plot(Ags, predPC2, 'r-','LineWidth', 2); 
+% xlim([0.5 1]); ylim([0.5 1]);
+% 
+% legend([h1 h2],{'true PC','predicted PC'},'Location', 'NorthWest');
+% xlabel('Ag'); 
+% ylabel('PC');
+% 
+% %for constructing AOCs, what if we went from measured Ag to predicted unbiased PC? 
