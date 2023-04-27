ced5212c9bc0ada39d78d4bb56b31d27d64b9d44
Alex L. White <alwhite@barnard.edu>
2023-04-27 11:30:03 -0400

    improvement to testSubplots which I use to remind myself of subplot ordering

--- testing/testSubplots.m
+++ testing/testSubplots.m
@@ -5,7 +5,7 @@ for rowi=1:nrows
     for coli=1:ncols
         subi=sub2ind([ncols nrows],coli,rowi); 
         subplot(nrows,ncols,subi);
-        text(1,1,sprintf('row=%i,col=%i,subi=%i',rowi,coli,subi));
+        text(0,0,sprintf('row=%i,col=%i,subi=%i',rowi,coli,subi), 'HorizontalAlignment','center');
         xlim([-3 3]); ylim([-2 2]);
     end
 end
